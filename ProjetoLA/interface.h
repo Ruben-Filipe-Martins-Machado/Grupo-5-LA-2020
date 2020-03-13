c7eea77208ed34ee5aca6974c9cf5228e52564c5
Grupo5LA <62101609+Grupo5LA@users.noreply.github.com>
2020-03-13 12:39:15 +0000

    Create interface.h

---
+++ ProjetoLA/interface.h
@@ -0,0 +1,10 @@
+#ifndef GUIAO5_INTERFACE_H
+#define GUIAO5_INTERFACE_H
+#include "dados.h"
+
+void mostrar_tabuleiro (ESTADO estado);
+
+int interpretador (ESTADO *e);
+
+
+#endif //GUIAO5_INTERFACE_H

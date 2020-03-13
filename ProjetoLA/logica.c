1ec3a96795bdd274cac74528623e989587fc9fad
Grupo5LA <62101609+Grupo5LA@users.noreply.github.com>
2020-03-13 12:36:31 +0000

    Create logica.c

---
+++ ProjetoLA/logica.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUF_SIZE 1024
+#include "dados.h"
+
+// Ainda por completar.
+int jogar (ESTADO *e, COORDENADA c) {
+    printf ("vez do jogador %d\n", e-> jogador_atual);
+    printf("jogar %d %d\n", c.coluna, c.linha);
+    return 1;
+}
